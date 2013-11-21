16d108a99ce8a4c6bf744db982284e0ab37d4d52
Makis Tracend <makis.tracend@gmail.com>
2013-11-20 17:32:44 -0800

    #1 adding condition to check if the page schema exists

--- models/Page.php
+++ models/Page.php
@@ -101,7 +101,7 @@ class Page extends Model {
 	}
 
 	// add the column if necessary
-	if( is_array($GLOBALS['db_schema']['pages']) && !in_array($key, $GLOBALS['db_schema']['pages']) ){
+	if( array_key_exists("pages", $GLOBALS['db_schema']) && is_array($GLOBALS['db_schema']['pages']) && !in_array($key, $GLOBALS['db_schema']['pages']) ){
 		$sql = "ALTER TABLE pages ADD COLUMN ". $key;
 		$results = $dbh->prepare($sql);
 		$results->execute();
