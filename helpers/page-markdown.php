4d712318a851fc5456a0347a32afe386d37bce23
Makis Tracend <makis.tracend@gmail.com>
2015-04-12 20:47:21 -0700

    :zap: using markdown version in edit

--- helpers/page-markdown.php
+++ helpers/page-markdown.php
@@ -5,9 +5,12 @@ use \Michelf\MarkdownExtra;
 class PageMarkdown {
 
 	public static function edit( &$data ){
-		// fallback - copy markdown from content if empty
-		if( !isset($data['markdown']) ) $data['markdown'] = "";
-		$data['content'] = $data['markdown'];
+		foreach( $data['body'] as $k => $body ){
+			// fallback - copy markdown from content if empty
+			if( !isset($data['body'][$k]['markdown']) ) $data['body'][$k]['markdown'] = "";
+			// present markdown version
+			$data['body'][$k]['content'] = $data['body'][$k]['markdown'];
+		}
 	}
 
 	public static function save( &$data ){
