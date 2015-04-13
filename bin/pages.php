eca407f2f08dcdf607abe2fa1a1e44afc3f74fbe
Makis Tracend <makis.tracend@gmail.com>
2015-04-12 19:51:19 -0700

    adding admin events

--- bin/pages.php
+++ bin/pages.php
@@ -1,12 +1,21 @@
 <?php
 
 //===============================================
-// Pages Initialization 
+// Pages Initialization
 //===============================================
 
-if( class_exists('Page') && method_exists(new Page(),'register')){ 
+if( class_exists('Page') && method_exists(new Page(),'register')){
 	// Register variables
 	Page::register("1", "markdown", "");
+}
+
+// add hooks
+if( class_exists('Controller') && method_exists('Controller','on') ){
+
+	Controller::on("admin:edit", 'PageMarkdown' );
+
+	Controller::on("admin:save", 'PageMarkdown' );
+
 }
 
 ?>
